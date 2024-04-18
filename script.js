98ae5ad25cce0317386c00de93d69c6566dea582
marine gazier <hazier.mg@gmail.com>
2024-04-18 17:01:49 +0200

    canvas mise en place

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
 const ctx = document.querySelector("#blackboard").getContext("2d");
 
+let width_blackboard = window.innerWidth;
+let height_blackboard = window.innerHeight;
+
 const init = () => {
   window.requestAnimationFrame(draw);
 }
 
 const draw = () => {
-  ctx.clearRect(0, 0, 1200, 900);
+  document.querySelector("#blackboard").style.width = window.innerWidth + "px";
+  document.querySelector("#blackboard").style.height = window.innerHeight + "px";
+
+  ctx.clearRect(0, 0, width_blackboard, height_blackboard);
   ctx.fillStyle = "black";
-  ctx.fillRect(0, 0, 1200, 900);
-
-
+  ctx.fillRect(0, 0, width_blackboard, height_blackboard);
   // window.requestAnimationFrame(draw);
 }
+
+init();
