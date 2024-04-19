86f66551e57dc36dac21de8b425805e9c447f1ea
marine gazier <hazier.mg@gmail.com>
2024-04-19 20:33:22 +0200

    background good enough...

--- script.js
+++ script.js
@@ -9,9 +9,8 @@ const draw = () => {
   document.querySelector("#blackboard").height = window.innerHeight -20;
 
   const gradient = ctx.createRadialGradient((window.innerWidth -20)/2, (window.innerHeight -20)/2, 0, (window.innerWidth -20)/2, (window.innerHeight -20)/2, (window.innerWidth -20));
-  gradient.addColorStop(0, "pink");
-  gradient.addColorStop(0.9, "white");
-  gradient.addColorStop(1, "green");
+  gradient.addColorStop(0, "rgba(0,0,0,0.9)");
+  gradient.addColorStop(1, "black");
 
   ctx.fillStyle = gradient;
   ctx.fillRect(0, 0, window.innerWidth -20, window.innerHeight -20);
