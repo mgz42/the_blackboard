bdbf796683183f88c61d02597df65dafe3f80f53
marine gazier <hazier.mg@gmail.com>
2024-04-20 14:17:41 +0200

    adding craie

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
 const ctx = document.querySelector("#blackboard").getContext("2d");
+const craie = document.querySelector(".craie");
+
+const update_pos_craie = (e) => {
+  // console.log(craie);
+  craie.style.top = (e.clientY - 25) + "px";
+  craie.style.left = e.clientX + "px";
+  craie.style.rotate = "-20deg";
+}
+
+const action_craie = () => {
+  window.addEventListener("mousemove", (e)=>{update_pos_craie(e)});
+}
+
+craie.addEventListener("click", ()=>{action_craie();});
 
 const init = () => {
   window.requestAnimationFrame(draw);
@@ -9,7 +23,7 @@ const draw = () => {
   document.querySelector("#blackboard").height = window.innerHeight -20;
 
   const gradient = ctx.createRadialGradient((window.innerWidth -20)/2, (window.innerHeight -20)/2, 0, (window.innerWidth -20)/2, (window.innerHeight -20)/2, (window.innerWidth -20));
-  gradient.addColorStop(0, "rgba(0,0,0,0.9)");
+  gradient.addColorStop(0, "rgba(0,0,0,0.86)");
   gradient.addColorStop(1, "black");
 
   ctx.fillStyle = gradient;
